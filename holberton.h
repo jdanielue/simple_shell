bc764887e524b60b43bd80bc3c183dbb6bf66852
jdanielue <jdurregoe@gmail.com>
2020-11-19 21:10:43 +0000

    nuevos headers y funciones

--- holberton.h
+++ holberton.h
@@ -7,11 +7,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 /* TOOLS_1 */
 char  **our_strtok(char *str, const char *delim);
 
 /* TOOLS_2 */
 int largo(char *string);
+void ultimonulo(char *linea);
+char *buscar_path(char **env);
+int num_tokens(char **str);
+
+/* TOOLS_3 */
 
 #endif
