395e833a1392d25726c7c9d19cd3f6d38e29a291
Sergio <2016@holbertonschool.com>
2020-11-25 16:47:21 -0500

    Add the non interactive mode

--- non_interactive_mode.c
+++ non_interactive_mode.c
@@ -9,7 +9,22 @@
 
 void non_interactive_mode(char **av, char **env)
 {
-	(void)env;
+	char **token_path = NULL;
+	char **token_line = NULL;
+	char *texto, *linea = NULL;
+	size_t largo_linea = 0;
+	const char *delim = " ";
+	int num_path;
 
-	write(1, av, 5);
+	getline(&linea, &largo_linea, stdin);
+	ultimonulo(linea);
+	linea = no_spaces(linea);
+	texto = buscar_path(env);
+	token_path = our_strtok(texto, ":");
+	num_path = num_tokens(token_path);
+	token_line = our_strtok(linea, delim);
+	add_command_path(num_path, token_path, token_line);
+	exc_process(token_line, token_path, av, env);
+	special_free(token_path);
+	special_free(token_line);
 }
