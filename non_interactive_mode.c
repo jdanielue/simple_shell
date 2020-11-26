fb1763a2371f3609c8372f254436ab6ae856ea2f
Sergio <2016@holbertonschool.com>
2020-11-25 23:46:09 -0500

    Fix the label

--- non_interactive_mode.c
+++ non_interactive_mode.c
@@ -2,11 +2,26 @@
 
 /**
  * non_interactive_mode - main of project shell
- * @av: Argument vector.
- * @env: The enviroment variables.
  * Return: No return.
  */
-
 void non_interactive_mode(void)
 {
+char **token_path = NULL;
+char **token_line = NULL;
+char *texto, *linea = NULL;
+size_t largo_linea = 0;
+const char *delim = " ";
+int num_path;
+
+getline(&linea, &largo_linea, stdin);
+ultimonulo(linea);
+linea = no_spaces(linea);
+texto = buscar_path();
+token_path = our_strtok(texto, ":");
+num_path = num_tokens(token_path);
+token_line = our_strtok(linea, delim);
+add_command_path(num_path, token_path, token_line);
+exc_process(token_line, token_path);
+special_free(token_path);
+special_free(token_line);
 }
