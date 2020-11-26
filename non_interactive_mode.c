aa33561424dd2314f6188f90d56f3c875a782496
Sergio <2016@holbertonschool.com>
2020-11-25 22:12:10 -0500

    Add non_interactive_mode.c function

--- non_interactive_mode.c
+++ non_interactive_mode.c
@@ -5,6 +5,24 @@
  * Return: No return
  */
 
-void non_interactive_mode(void)
+void non_interactive_mode(char **av, char **env)
 {
+char **token_path = NULL;
+char **token_line = NULL;
+char *texto, *linea = NULL;
+size_t largo_linea = 0;
+const char *delim = " ";
+int num_path;
+
+getline(&linea, &largo_linea, stdin);
+ultimonulo(linea);
+linea = no_spaces(linea);
+texto = buscar_path(env);
+token_path = our_strtok(texto, ":");
+num_path = num_tokens(token_path);
+token_line = our_strtok(linea, delim);
+add_command_path(num_path, token_path, token_line);
+exc_process(token_line, token_path, av, env);
+special_free(token_path);
+special_free(token_line);
 }
